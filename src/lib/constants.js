1d272fe3627898e4294a04c7e0b8793ed0babb03
Charlie Page <charles.david.page@gmail.com>
2025-05-16 06:48:21 -0500

    Updating image links

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,8 @@
 export const Images = {
-    journalCover: '/lib/images/Horizon_Cover.jpg',
-    travelCompanion: '/lib/images/Horizon_Packing_List.jpg',
-    guidedJournal: '/lib/images/Horizon_Return.jpg',
-    travelKeepsake: '/lib/images/Horizon_Best_Moments.jpg',
+    logo: '/images/logo.png',
+    journalCover: '/images/Horizon_Cover.jpg',
+    travelCompanion: '/images/Horizon_Packing_List.jpg',
+    guidedJournal: '/images/Horizon_Return.jpg',
+    travelKeepsake: '/images/Horizon_Best_Moments.jpg',
     placeholder: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
 }; 
